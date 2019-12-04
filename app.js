309fcf57005cf7da8c573530bbd5d14381e803c0
--unset <peter.heinum@gmail.com>
2019-12-04 22:12:27 +0100

    added pitchjs hopefully we can play with that and now colors sync smoothly with sound

--- app.js
+++ app.js
@@ -5,33 +5,12 @@ var audioInput = null,
   realAudioInput = null,
   inputPoint = null,
   audioRecorder = null
-var rafID = null
 var analyserContext = null
 var canvasWidth, canvasHeight
 var recIndex = 0
 
-/* TODO:
-- offer mono option
-- "Monitor input" switch
-*/
-
-function saveAudio() {
-  audioRecorder.exportWAV(doneEncoding)
-  // could get mono instead by saying
-  // audioRecorder.exportMonoWAV( doneEncoding )
-}
-
 function gotBuffers(buffers) {
-  var canvas = document.getElementById("wavedisplay")
-  drawBuffer(canvas.width, canvas.height, canvas.getContext('2d'), buffers[0])
-  // the ONLY time gotBuffers is called is right after a new recording is completed - 
-  // so here's where we should set up the download.
-  // audioRecorder.exportWAV(doneEncoding)
-}
-
-function doneEncoding(blob) {
-  Recorder.setupDownload(blob, "myRecording" + ((recIndex < 10) ? "0" : "") + recIndex + ".wav")
-  recIndex++
+  drawBuffer(buffers[0])
 }
 
 setInterval(() => {
@@ -40,89 +19,8 @@ setInterval(() => {
     audioRecorder.stop()
     audioRecorder.getBuffers(gotBuffers)
     audioRecorder.clear()
-  }, 300);
-}, 400);
-
-function toggleRecording(e) {
-  if (e.classList.contains("recording")) {
-    // stop recording
-    audioRecorder.stop()
-    e.classList.remove("recording")
-    audioRecorder.getBuffers(gotBuffers)
-  } else {
-    // start recording
-    if (!audioRecorder)
-      return
-    e.classList.add("recording")
-    audioRecorder.clear()
-    audioRecorder.record()
-  }
-}
-
-function convertToMono(input) {
-  var splitter = audioContext.createChannelSplitter(2)
-  var merger = audioContext.createChannelMerger(2)
-  
-  input.connect(splitter)
-  splitter.connect(merger, 0, 0)
-  splitter.connect(merger, 0, 1)
-  return merger
-}
-
-function cancelAnalyserUpdates() {
-  window.cancelAnimationFrame(rafID)
-  rafID = null
-}
-
-function updateAnalysers(time) {
-  if (!analyserContext) {
-    var canvas = document.getElementById("analyser")
-    canvasWidth = canvas.width
-    canvasHeight = canvas.height
-    analyserContext = canvas.getContext('2d')
-  }
-  
-  // analyzer draw code here
-  var SPACING = 5
-  var BAR_WIDTH = 4
-  var numBars = Math.round(canvasWidth / SPACING)
-  
-  var freqByteData = new Uint8Array(analyserNode.frequencyBinCount)
-  analyserNode.getByteFrequencyData(freqByteData)
-  analyserContext.clearRect(0, 0, canvasWidth, canvasHeight)
-  analyserContext.fillStyle = '#F6D565'
-  analyserContext.lineCap = 'round'
-  var multiplier = analyserNode.frequencyBinCount / numBars
-  // Draw rectangle for each frequency bin.
-  for (var i = 0; i < numBars; ++i) {
-    var magnitude = 0
-    var offset = Math.floor(i * multiplier)
-    // gotta sum/average the block, or we miss narrow-bandwidth spikes
-    for (var j = 0; j < multiplier; j++)
-      magnitude += freqByteData[offset + j]
-    magnitude = magnitude / multiplier
-    var magnitude2 = freqByteData[i * multiplier]
-    analyserContext.fillStyle = "hsl( " + Math.round((i * 360) / numBars) + ", 100%, 50%)"
-    analyserContext.fillRect(i * SPACING, canvasHeight, BAR_WIDTH, -magnitude)
-  }
-
-  rafID = window.requestAnimationFrame(updateAnalysers)
-}
-
-function toggleMono() {
-  if (audioInput != realAudioInput) {
-    audioInput.disconnect()
-    realAudioInput.disconnect()
-    audioInput = realAudioInput
-  } else {
-    realAudioInput.disconnect()
-    audioInput = convertToMono(realAudioInput)
-  }
-
-  audioInput.connect(inputPoint)
-}
-
-
+  }, 900);
+}, 1000);
 
 function gotStream(stream) {
   inputPoint = audioContext.createGain()
@@ -131,9 +29,7 @@ function gotStream(stream) {
   realAudioInput = audioContext.createMediaStreamSource(stream)
   audioInput = realAudioInput
   audioInput.connect(inputPoint)
-
-  //    audioInput = convertToMono( input )
-
+  
   analyserNode = audioContext.createAnalyser()
   analyserNode.fftSize = 2048
   inputPoint.connect(analyserNode)
@@ -144,8 +40,6 @@ function gotStream(stream) {
   zeroGain.gain.value = 0.0
   inputPoint.connect(zeroGain)
   zeroGain.connect(audioContext.destination)
-
-  updateAnalysers()
 }
 
 function initAudio() {
@@ -155,20 +49,7 @@ function initAudio() {
     navigator.cancelAnimationFrame = navigator.webkitCancelAnimationFrame || navigator.mozCancelAnimationFrame
   if (!navigator.requestAnimationFrame)
     navigator.requestAnimationFrame = navigator.webkitRequestAnimationFrame || navigator.mozRequestAnimationFrame
-  navigator.getUserMedia({
-    "audio": {
-      "mandatory": {
-        "googEchoCancellation": "false",
-        "googAutoGainControl": "false",
-        "googNoiseSuppression": "false",
-        "googHighpassFilter": "false"
-      },
-      "optional": []
-    },
-  }, gotStream, function (e) {
-    alert('Error getting audio')
-    console.log(e)
-  })
+
   navigator.getUserMedia(
     {
       "audio": {
@@ -182,6 +63,9 @@ function initAudio() {
       },
     }, gotStream, function (e) {
       alert('Error getting audio')
+      setTimeout(() => {
+        initAudio()
+      }, 2000);
       console.log(e)
     })
 }
--- recorderjs/audiodisplay.js
+++ recorderjs/audiodisplay.js
@@ -1,39 +1,51 @@
 const get = async ({ url, body, method }) => await fetch(url, { body: JSON.stringify(body), method }).then(res => res.json())
 
-let lastColor = 0
+let _temperature = 0
+let _color = 0
 
 const getLights = async ({ id, temperature }) => {
   const url = `http://192.168.1.99/api/aQhjtMtA9Cg76LpuGRb55WRMThjj-TLrvMdBTPzh/lights/${id}/`
   const method = 'GET'
-  const response = await get({ url, method })
+  get({ url, method })
 }
 
-const setLights = async ({ id, temperature, mirror }) => {
-  
-  if(temperature - lastColor > 0.05 || temperature - lastColor < -0.05) {
-    console.log(lastColor, temperature)
+const setLights = async ({ id, temperature, color }) => {
+  if (temperature - _temperature > 0.05 || temperature - _temperature < -0.05) {
     const url = `http://192.168.1.99/api/aQhjtMtA9Cg76LpuGRb55WRMThjj-TLrvMdBTPzh/lights/${id}/state`
-    
-    const color = Math.floor(temperature * 65535)
-    
-    const body = mirror
-    ?  { 'on': true, 'sat': 254, 'bri': Math.floor(200*temperature), "hue": color }
-    : { 'on': true, 'sat': 254, 'bri': Math.floor(200*temperature), 'hue': color }
+
+    const body = { 'on': true, 'sat': 254, 'bri': Math.floor(200 * temperature), "hue": color }
+    console.log(color)
     const method = 'PUT'
-    const response = await get({ url, body, method })
-    lastColor = temperature
+    get({ url, body, method })
   }
 }
 
 
-function drawBuffer(width, height, context, data) {
+function drawBuffer(data) {
   let temperature = 0;
 
-  for(let i = 0; i < data.length; i++){
-  if(temperature < data[i]) {
-    temperature = data[i];
-  }}
-  // console.log(temperature)
-  setLights({ id: 1, temperature })
-  setLights({ id: 2, temperature, mirror: true })
+  for (let i = 0; i < data.length; i++) {
+    if (temperature < data[i]) {
+      temperature = data[i];
+    }
+  }
+  let color = 0
+
+  if(temperature > 0.5 || _color + Math.floor(_color * temperature) < 1500) {
+    color = _color + Math.floor(_color * temperature)
+  } 
+  if(temperature < 0.5 || _color + Math.floor(_color * temperature) > 65535) {
+    color = _color - Math.floor(_color * temperature)
+  }
+
+  if(color < 1500 || color > 65535) {
+    console.log('saved color', color)
+    console.log(temperature)
+    color = Math.floor(temperature * 65535)
+  }
+  
+  setLights({ id: 1, temperature, color })
+  setLights({ id: 2, temperature, color })
+  _color = color
+  _temperature = temperature
 }
