f9a03e75cc4206a09da4f372facae3fa56e4f683
Joakim Allen <joakim.allen@greaterthan.eu>
2019-12-12 19:41:05 +0100

    forgot about these files

--- init.js
+++ init.js
@@ -1,20 +1,41 @@
 const fs = require('fs');
+const https = require('https');
 
-const get = async ({
-  url,
-  body,
-  method
+const get = ({
+  url
 }) => {
-  return Promise.resolve()
+  // https package isn't async so a promise wrap is required for
+  // callers using this method with await/async methods
+  return new Promise((resolve, reject) => {
+    let data = '';
+    https.get(url, (resp) => {
+
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      resp.on('end', () => {
+        resolve(JSON.parse(data))
+      });
+
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+      reject(err)
+    });
+  })
+
 }
 
 const get_internal_ip = async (debug = false) => {
 
   if (debug) {
-    return Promise.resolve("localhost")
+    return Promise.resolve({
+      status: 200,
+      internalipaddress: "127.0.0.1"
+    })
   }
 
-  const url = 'https://discovery.meethue.com/'
+  const url = 'https://discovery.meethue.com'
   const [res] = await get({
     url,
     method: 'GET'
@@ -25,11 +46,15 @@ const get_internal_ip = async (debug = false) => {
       internalipaddress
     } = res
 
-    return Promise.resolve(internalipaddress)
+    return Promise.resolve({
+      internalipaddress,
+      status: 200
+    })
   } else {
-    create_error({
-      text: 'No hue found',
-      font_size: 18
+    console.log("Error: No Hue hub found!\n")
+    return Promise.resolve({
+      status: 404,
+      internalipaddress: ''
     })
   }
 }
@@ -37,15 +62,23 @@ const get_internal_ip = async (debug = false) => {
 const write_env_file = async (ip) => {
   const line = `INTERNALIPADDRESS=${ip}`
   fs.writeFile('.env', line, (err) => {
-    // throws an error, you could also catch it here
     if (err) throw err;
 
-    // success case, the file was saved
-    console.log("Setting internal ip address to " + ip)
+    console.log('Setting env variable "INTERNALIPADDRESS" to ' + ip)
     return Promise.resolve()
   });
 }
 
-get_internal_ip(true).then(async res => {
-  write_env_file(res)
-})
+async function init() {
+  const DEBUG = true
+
+  const result = await get_internal_ip(DEBUG)
+
+  if (result.status != 200) {
+    return
+  }
+
+  write_env_file(result.internalipaddress)
+}
+
+init()
